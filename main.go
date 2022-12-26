803d91a1503ffecf7104e4d2029254dd978b8482
thangsuperman <thangjenny2002@gmail.com>
2022-12-26 14:50:14 +0700

    feat: added the method for getting all of the keys and values in redis

--- main.go
+++ main.go
@@ -29,10 +29,20 @@ func main() {
 	haltOn(err)
 	fmt.Println("inter :>> ", inter)
 
-	title, err := redis.String(conn.Do("HGET", "podcast:1", "title"))
-	haltOn(err)
-	fmt.Println("title :>> ", title)
+	// title, err := redis.String(conn.Do("HGET", "podcast:1", "title"))
+	// haltOn(err)
+	// fmt.Println("title :>> ", title)
 
-	membershipFee, err := redis.Float64(conn.Do("HGET", "podcast:1", "membership_fee"))
-	fmt.Println("membershipFee :>> ", membershipFee)
+	// membershipFee, err := redis.Float64(conn.Do("HGET", "podcast:1", "membership_fee"))
+	// haltOn(err)
+	// fmt.Println("membershipFee :>> ", membershipFee)
+
+	values, err := redis.StringMap(conn.Do("HGETALL", "podcast:1"))
+	haltOn(err)
+
+	// Loop every single item inside the map
+	for k, v := range values {
+		fmt.Println("Key :>> ", k)
+		fmt.Println("Value :>> ", v)
+	}
 }
